c85a7487de2046f44b41872af589ab60112cbea2
耿昌兵 <1174968760@qq.com>
2019-09-11 16:39:44 +0800

    增加模块

---
+++ gmall-order-web/src/main/java/com/atguigu/gmall/controller/OrderController.java
@@ -0,0 +1,37 @@
+package com.atguigu.gmall.controller;
+
+import com.atguigu.gmall.UserAddress;
+import com.atguigu.gmall.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+
+
+@RestController
+public class OrderController {
+    @Autowired
+    private UserInfoService userInfoService;
+    @RequestMapping("trade")
+    public String trade(){
+        // 返回一个视图名称叫index.html
+        return "index";
+    }
+
+
+
+
+
+
+    @RequestMapping("trade")
+    @ResponseBody // 第一个返回json 字符串，fastJson.jar 第二直接将数据显示到页面！
+    public List<UserAddress> trade(String userId){
+        // 返回一个视图名称叫index.html
+        return userInfoService.getUserAddressList(userId);
+    }
+
+}
